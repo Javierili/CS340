78b1baf414912cf536ef44fb05d2212c05f8dffc
Javierili <javieri@oregonstate.edu>
2019-02-09 18:21:29 -0800

    Added Table creation SQL file
    To run this in Putty: 
    1. Download this file into your engineering server 
    2. Connect to the database in putty
    3. run command " source ~/[PATH TO THE FILE] "
    
    Confirm table creation by running "show tables;" command

---
+++ TableCreation.sql
@@ -0,0 +1,114 @@
+-- MySQL dump 10.13  Distrib 5.1.66, for redhat-linux-gnu (x86_64)
+--
+-- Host: mysql.eecs.oregonstate.edu    Database: CS275
+-- ------------------------------------------------------
+-- Server version	5.1.65-community-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bsg_planets`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `major` varchar(3) DEFAULT NULL,
+  `building` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY (`building`),
+  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`building`) REFERENCES `buildings` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `bsg_cert`
+--
+
+DROP TABLE IF EXISTS `professor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `professor` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `college` varchar(5) NOT NULL,
+  `tenured` int(1),
+  `building` varchar(5),
+  `boss` int(9),
+  PRIMARY KEY (`id`),
+  KEY (`building`),
+  CONSTRAINT `professor_ibfk_1` FOREIGN KEY (`building`) REFERENCES `buildings` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `bsg_people`
+--
+
+DROP TABLE IF EXISTS `class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `class` (
+  `course_id` int(5) NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(50) NOT NULL,
+  `college` varchar(3) DEFAULT NULL,
+  `professor` int(9) DEFAULT NULL,
+  `building_name` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`course_id`),
+  KEY `professor` (`professor`),
+  CONSTRAINT `class_ibfk_1` FOREIGN KEY (`professor`) REFERENCES `professor` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `class_ibfk_2` FOREIGN KEY (`building_name`) REFERENCES `building` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `bsg_cert_people`
+--
+
+DROP TABLE IF EXISTS `students_classes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students_classes` (
+  `student_id` int(9) NOT NULL DEFAULT '0',
+  `class_id` int(5) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`student_id`,`class_id`),
+  KEY `student_id` (`student_id`),
+  CONSTRAINT `students_classes_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`),
+  CONSTRAINT `students_classes_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `class` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `building`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `building` (
+  `name` varchar(5) NOT NULL,
+  `rooms` int(5),
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-02-04 12:54:40
